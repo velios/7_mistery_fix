04cb1a3799cf7bb4265febb9fa031f8ec9cf9dd4
velios <prostozapomnit@gmail.com>
2017-08-05 22:09:12 +0300

    Исправлен баг

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -3,9 +3,11 @@ from math import sqrt
 
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
-    root1 = (-b - sqrt(discriminant)) / (2 * a)
-    root2 = (-b + sqrt(discriminant)) / (2 * a)
+    root1 = (-b - sqrt(abs(discriminant))) / (2 * a)
+    root2 = (-b + sqrt(abs(discriminant))) / (2 * a)
     if discriminant == 0:
         return root1, None
-    else:
+    elif discriminant > 0:
         return root1, root2
+    else:
+        return None, None
